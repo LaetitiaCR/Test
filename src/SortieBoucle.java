fb823ec21d2d554cf33979dc4b1e0dee09c7c67c
LaetitiaCR <l.carrivale@laposte.net>
2020-01-16 08:31:23 +0100

    division par 0 sortie de boucle

---
+++ src/SortieBoucle.java
@@ -0,0 +1,15 @@
+
+public class SortieBoucle {
+
+	public static void main(String[] args) {
+		for(int x= -5; x<=5; x++) {
+			if(x==0) {
+				System.out.println("division par 0");
+				break;
+			}
+			System.out.println(1.0/x);
+		}
+
+	}
+
+}
